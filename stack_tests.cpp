ae7ff0a9ee2cceabe30ca933833bf3d09ab3934e
userCpP <mtszplcht@gmail.com>
2021-06-05 01:51:13 +0200

    commit

--- stack_tests.cpp
+++ stack_tests.cpp
@@ -1,42 +1,72 @@
 #include <gtest/gtest.h>
 #include "../src/stack.h"
 
-Stack stack;
 
 TEST(Tests, tc1){
+    Stack stack;
     ASSERT_EQ(0, stack.pop());
 }
 
 TEST(Tests, tc2){
+    Stack stack;
     ASSERT_EQ(true, stack.isEmpty());
 }
 
 TEST(Tests, tc3){
+    Stack stack;
     stack.push(1);
     stack.push(2);
     ASSERT_EQ(2, stack.pop());
 }
 
 TEST(Tests, tc4){
+    Stack stack;
+    stack.push(1);
+    stack.push(2);
+    stack.pop();
     ASSERT_EQ(false, stack.isEmpty());
 }
 
 TEST(Tests, tc5){
+    Stack stack;
+    stack.push(1);
+    stack.push(2);
+    stack.pop();
     stack.push(5);
     ASSERT_EQ(2, stack.size());
 }
 
 TEST(Tests, tc6){
+    Stack stack;
+    stack.push(1);
+    stack.push(2);
+    stack.pop();
+    stack.push(5);
+    stack.pop();
     stack.push(12);
-    ASSERT_EQ(3, stack.size());
+    ASSERT_EQ(2, stack.size());
 }
 
 TEST(Tests, tc7){
+    Stack stack;
+    stack.push(1);
+    stack.push(2);
+    stack.pop();
+    stack.push(5);
+    stack.pop();
+    stack.push(12);
     stack.push(14);
     ASSERT_EQ(14, stack.pop());
 }
 
 TEST(Tests, tc8){
+    Stack stack;
+    stack.push(1);
+    stack.push(2);
+    stack.pop();
+    stack.push(5);
+    stack.pop();
+    stack.push(12);
     stack.push(14);
     ASSERT_EQ(false, stack.isEmpty());
 }
